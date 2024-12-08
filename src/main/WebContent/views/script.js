70da11db458a49ac8e5af8b7b06e3b295e805228
Trần Phi Hùng <phihung1701@gmail.com>
2024-12-08 09:12:50 +0700

    sua lai thu vien

--- src/main/WebContent/views/script.js
+++ src/main/WebContent/views/script.js
@@ -1,5 +1,5 @@
 
-const socket = new WebSocket("ws://localhost:8080/ConvertPDFToWord/socket");
+const socket = new WebSocket("ws://localhost:8085/ConvertPDFToWord/socket");
 
 socket.onopen = function() {
 	console.log("WebSocket connection established.");
